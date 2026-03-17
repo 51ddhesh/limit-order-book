7a04734300ad9a27aa83bc2d11a0433c4cd48d91
siddhesh <173941233+51ddhesh@users.noreply.github.com>
2026-03-17 20:01:51 +0000

    scaffold  memory allocator

---
+++ include/lob/memory/pool_allocator.hpp
@@ -0,0 +1,33 @@
+// Copyright 2026 51ddhesh
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+/*
+    lob::memory::pool_allocator
+    Fixed size object pool
+*/
+
+/*
+    Preallocates storage for N objects of type T at construction time.
+*/
+
+#include <cassert>
+#include <type_traits>
+#include <utility>
+
+namespace lob {
+
+template <typename T>
+class PoolAllocator {
+    static_assert(std::is_trivially_destructible_v<T>);
+
+private:
+    std::byte* storage_; // contiguous block for `capacity_` objects
+    T** free_stack_; // LIFO stack of pointers to free slots
+    std::size_t capacity_; // total number of slots
+    std::size_t free_top_; // number of entries currently in the free_stack_    
+
+}; // class lob::PoolAllocator
+
+} // namespace lob
